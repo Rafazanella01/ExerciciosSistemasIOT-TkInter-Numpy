e94a8f8755b7e6d3b655d08f03d20ddea70be389
Rafazanella01 <rafazanella2004@gmail.com>
2024-08-28 19:18:40 -0300

    Update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rafaz\OneDrive\Área de Trabalho\ExerciciosSistemasIOT-TkInter-Numpy\venv
+set VIRTUAL_ENV=C:\Users\rafaz\OneDrive\Área de Trabalho\Codagem\ExerciciosSistemasIOT-TkInter-Numpy\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
