a6f2c5dc2d693bb17f7565e7d12d5216cef44e88
Rafazanella01 <rafazanella2004@gmail.com>
2024-08-28 20:03:25 -0300

    Update

--- Exercicio1.py
+++ Exercicio1.py
@@ -1,2 +1,18 @@
 import pandas as pd
 import numpy as np
+
+def meancenter(matriz):
+    
+    media = np.mean(matriz, axis=0)
+    
+    out = matriz - media
+    
+    return out
+
+def autoscale(dados):
+    media = np.mean(dados, axis=0)
+    desvioPadrao = np.std(dados, axis=0)
+    
+    dadosEscalados = (dados - media) / desvioPadrao
+    
+    return dadosEscalados
